e3e01aafaae7dd7f5dde90a2843a6031595007d5
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-08-25 13:44:02 -0500

    Fix build

--- src/test/java/codebreaker/CodeBreakerSteps.java
+++ src/test/java/codebreaker/CodeBreakerSteps.java
@@ -3,6 +3,8 @@ package codebreaker;
 import cuke4duke.Given;
 
 public class CodeBreakerSteps {
+    private Game game;
+    
     @Given("the secret code is (.*)")
     public void theSecretCodeIs(String code) {
         game = new Game(code);
