a327f304bd84dfb95a91213cd60e5c4984adf18a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-08-27 11:20:57 -0500

    Game working

--- src/main/java/codebreaker/Game.java
+++ src/main/java/codebreaker/Game.java
@@ -1,11 +1,35 @@
 package codebreaker;
 
+import java.util.Arrays;
+
 public class Game {
+    private char[] secret;
+
     public Game(String code) {
-        
+        this.secret = code.toCharArray();
     }
 
-    public String guess(String code) {
-        return "fixme";
+    public String guess(String guessString) {
+        String mark = "";
+        char[] guess = guessString.toCharArray();
+        for(int guessPos = 0; guessPos < 4; guessPos++) {
+            if(secret[guessPos] == guess[guessPos]) {
+                mark += 'b';
+                secret[guessPos] = '_';
+            }
+        }
+        for(int guessPos = 0; guessPos < 4; guessPos++) {
+            for(int secretPos = 0; secretPos < 4; secretPos++) {
+                if(secret[secretPos] == guess[guessPos]) {
+                    mark += 'w';
+                    secret[secretPos] = '_';
+                    break;
+                }
+            }
+        }
+        
+        char[] chars = mark.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
--- src/test/java/codebreaker/CodeBreakerSteps.java
+++ src/test/java/codebreaker/CodeBreakerSteps.java
@@ -3,22 +3,28 @@ package codebreaker;
 import cuke4duke.*;
 import static org.junit.Assert.*;
 
+import java.util.regex.Pattern;
+
 public class CodeBreakerSteps {
     private Game game;
     private String mark;
     
     @Given("the secret code is (.*)")
     public void theSecretCodeIs(String code) {
-        game = new Game(code);
+        game = new Game(stripSpace(code));
     }
 
     @When("I guess (.*)")
     public void iGuess(String code) {
-        mark = game.guess(code);
+        mark = game.guess(stripSpace(code));
     }
 
     @Then("the mark should be (.*)")
     public void theMarkShouldBe(String code) {
         assertEquals(code, mark);
+    }
+
+    private String stripSpace(String s) {
+       return s.replaceAll(" ", "");        
     }
 }
